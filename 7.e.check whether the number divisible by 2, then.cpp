6ebab39c966198a3e6f7621456d7907082f775db
Dharmendrapoondla <122214720+Dharmendrapoondla@users.noreply.github.com>
2023-01-09 14:37:41 +0530

    Add files via upload

---
+++ 7.e.check whether the number divisible by 2, then.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ int i;
+printf("enter a num:");
+scanf("%d",&i);
+if(i%2==0)
+printf("the num is even");
+else
+printf("The num is odd");
+}
---
+++ 8.e. print numbers from P to Q but except.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{int i,a,p,q,r;
+printf("enter start range");
+scanf("%d",&p);
+printf("enter end range");
+scanf("%d",&r);
+printf("the exjSBFA");
+scanf("%d",&q);
+for(i=p;i<=r;i++)
+{if(i==q)
+continue;
+
+printf("%d",i);
+}
+}
